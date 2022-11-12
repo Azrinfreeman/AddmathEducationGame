ad698b498d3f6c975892d661c2fc2878cfea20ea
Azrinfreeman <azrinfreeman2@gmail.com>
2022-11-12 14:26:54 +0800

    2 scenes added
    Login Page and Main Menu Scenes added

---
+++ Assets/ButtonAnimationController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class ButtonAnimationController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void OnPointerDown(PointerEventData eventData){
+ animator.SetTrigger("isClicking");   
+}
+ 
+    public void OnPointerUp(PointerEventData eventData){
+ animator.SetTrigger("isClicking");
+}
+}
