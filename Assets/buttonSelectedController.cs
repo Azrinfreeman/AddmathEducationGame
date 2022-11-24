6ab8719903e1d1a59f091d288a37049f18a1e98b
Azrinfreeman <azrinfreeman2@gmail.com>
2022-11-24 14:51:43 +0800

    24/11/2022

--- Assets/buttonSelectedController.cs
+++ Assets/buttonSelectedController.cs
@@ -9,12 +9,81 @@ public class buttonSelectedController : MonoBehaviour
     void Start()
     {
        for(int i =0; i < 4; i++){
-        answers[i] = transform.GetChild(i).GetComponent<Button>();
+        answers[i] = GameObject.Find("Answers").transform.GetChild(i).GetComponent<Button>();
+        answers[i].onClick.AddListener(selectedButton(1));
        }
+
+       
     }
 
-    public void selectedButtonA(){
-        
+    public void selectedButton(int index){
+        ColorBlock colors;
+        ColorBlock colors2;
+        for(int i = 0; i <4; i++){
+            colors = answers[i].colors;
+            colors.normalColor = new Color32(255, 255, 255, 255);
+            
+            if(i!=index){
+                answers[i].colors = colors;
+            }
+            
+        }
+        colors2 = answers[index].colors;
+        colors2.normalColor = new Color32(255,217,1,255);
+        colors2.selectedColor = new Color32(255,217,1,255);
+        answers[index].colors = colors2;
+    }
+    public void selectedButtonB(){
+       ColorBlock colors;
+        ColorBlock colors2;
+        for(int i = 0; i <4; i++){
+            colors = answers[i].colors;
+            colors.normalColor = new Color32(255, 255, 255, 255);
+            
+            if(i!=1){
+                answers[i].colors = colors;
+            }
+            
+        }
+        colors2 = answers[1].colors;
+        colors2.normalColor = new Color32(255,217,1,255);
+        colors2.selectedColor = new Color32(255,217,1,255);
+        answers[1].colors = colors2;
+    }
+    public void selectedButtonC(){
+        ColorBlock colors;
+        ColorBlock colors2;
+        for(int i = 0; i <4; i++){
+            colors = answers[i].colors;
+            colors.normalColor = new Color32(255, 255, 255, 255);
+            
+            if(i!=2){
+                answers[i].colors = colors;
+            }
+            
+        }
+        colors2 = answers[2].colors;
+        colors2.normalColor = new Color32(255,217,1,255);
+        colors2.selectedColor = new Color32(255,217,1,255);
+        answers[2].colors = colors2;
+    }
+
+    public void selectedButtonD(){
+    ColorBlock colors;
+        ColorBlock colors2;
+        for(int i = 0; i <4; i++){
+            colors = answers[i].colors;
+            colors.normalColor = new Color32(255, 255, 255, 255);
+            
+            if(i!=3){
+                answers[i].colors = colors;
+            }
+            
+        }
+        colors2 = answers[3].colors;
+        colors2.normalColor = new Color32(255,217,1,255);
+        colors2.selectedColor = new Color32(255,217,1,255);
+        answers[3].colors = colors2;
     }
     // Update is called once per frame
     void Update()
