c13b19b00f3867277e0212b21d547cc2a0946cae
Azrinfreeman <azrinfreeman2@gmail.com>
2022-12-06 23:53:37 +0800

    06/12/2022 2

--- Assets/AlgoController.cs
+++ Assets/AlgoController.cs
@@ -6,17 +6,39 @@ public class AlgoController : MonoBehaviour
 {
     public Transform[] input;
     public int numInput;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(transform.GetChild(1).transform.name);
-        //numInput = transform.GetChild(1).name;
-        //input = new Transform[numInput];
+        
+        numInput = transform.GetChild(1).transform.childCount;;
+        input = new Transform[numInput];
     }
 
+    public void CheckingTheInput(){
+        for(int i = 0; i < numInput; i++){
+            for(int l=0; l < transform.childCount; l++){
+                if(!input[l].transform.GetChild(l).gameObject.activeInHierarchy){
+                   
+                }else{
+                    Debug.Log("do nothing");
+                }
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+     private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "arrow"){
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        Debug.Log("exit");
     }
 }
