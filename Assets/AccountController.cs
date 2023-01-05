d72277d9b56cb0eab4b1598db891a870cc2aee8c
Azrinfreeman <azrinfreeman2@gmail.com>
2023-01-05 10:48:02 +0800

    5/1

--- Assets/AccountController.cs
+++ Assets/AccountController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class AccountController : MonoBehaviour
 {
     public Transform body;
@@ -17,6 +18,11 @@ public class AccountController : MonoBehaviour
         for(int i = 0; i < PlayerPrefs.GetInt("PlayerTotal"); i++){
             Transform acc = Instantiate(Account, new Vector2(Account.position.x + (990f*i), Account.localPosition.y+822), Account.rotation);
             acc.transform.SetParent(body);
+            acc.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetString("PlayerName_"+i);
+            acc.GetChild(0).transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetString("PlayerGender_"+i);
+            acc.GetChild(0).transform.GetChild(3).GetComponent<AssignID>().id = PlayerPrefs.GetInt("PlayerID_"+i);
+            
+            
         }
 
         Debug.Log(PlayerPrefs.GetInt("PlayerTotal"));;
